061394eadce1d080c580636deae8a9c562d7132a
Kevin Vallejo <kevinvallejo@Kevins-MacBook-Air.local>
2017-02-06 18:39:43 -0500

    take 2

--- unit5/strings.cpp
+++ unit5/strings.cpp
@@ -6,4 +6,40 @@
 //  Copyright © 2017 Kevin Vallejo. All rights reserved.
 //
 
-#include "strings.hpp"
+#include "strings.h"
+#include "iostream"
+#include "stdio.h"
+
+using namespace std;
+bool palindrome(const string & s)
+{
+    cout << "palindrome called" << endl;
+    int i = 0;
+    int j = 1;
+    
+    for (i=0; i< s.length(); i++) {
+        if(s[i] == s[s.length()-j]){
+            
+        }else{
+            cout << "does not match" << endl;
+            return false;
+        }
+        j++;
+    }
+    return true;
+}
+
+string replace_all(const string & orignal_string, const string & old_substring, const string & new_substring)
+{
+    string s = orignal_string;
+  
+    
+    int position = (int)s.find(old_substring);
+    if (position != string::npos){
+        s.replace(position, old_substring.length(), new_substring);
+    } else{
+        cout << old_substring << " not found\n";
+    }
+  
+    return s;
+}
